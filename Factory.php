cfbae008b9d55c6d859cab3f6b1d3cebd45ed9a5
tolves <tov.sharing@gmail.com>
2020-12-07 18:51:23 +0000

    Make a factory class, which is able to fetch the data from the tables and return the appropriate model class. Update the factory class, add a fetch function which takes an argument of the model class and returns it.

---
+++ Factory.php
@@ -0,0 +1,52 @@
+<?php
+require_once('DBconnection.php');
+require_once ('models/account_abstract.php');
+class Factory
+{
+    public static function build($table)
+    {
+        switch ($table) {
+            case 'user':
+                $obj = new User();
+                break;
+            case 'user_admin':
+                $obj = new User_admin();
+                break;
+            case 'user_agent':
+                $obj = new User_agent();
+                break;
+        }
+        return $obj;
+    }
+
+    public static function fetch($table, $argument, $value)
+    {
+        $obj = Factory::build($table);
+        $obj->fetch($argument, $value);
+        return $obj;
+    }
+
+}
+
+$obj = Factory::build('user');
+$obj->fetch('id', '1');
+echo $obj->getId();
+echo '<br/>';
+echo $obj->getName();
+echo '<br/>';
+echo $obj->getEmail();
+echo '<br/>';
+echo $obj->getCreatedAt();
+echo '<br/>';
+$obj = Factory::fetch('user', "name", "test");
+echo '<br/>';
+var_dump($obj);
+echo '<br/>';
+echo $obj->getId();
+echo '<br/>';
+echo $obj->getName();
+echo '<br/>';
+echo $obj->getEmail();
+echo '<br/>';
+echo $obj->getCreatedAt();
+echo '<br/>';
