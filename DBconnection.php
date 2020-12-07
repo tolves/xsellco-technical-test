7e40c04d469829cd6a4b76894cd708bffed2f849
tolves <tov.sharing@gmail.com>
2020-12-07 18:50:07 +0000

    Create a class DBConnection using the Singleton design pattern which will handle the connection to the database.

---
+++ DBconnection.php
@@ -0,0 +1,51 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class DatabaseSingleton
+{
+    private static $instance = null;
+
+    private function __construct()
+    {
+        $conn_string = "host=localhost port=5432 dbname=xsellco user=postgres password=123456";
+        $this->connection = pg_connect($conn_string);
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (null === self::$instance) {
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    private $connection = null;
+
+    public function query($sql)
+    {
+//        $sql = pg_escape_string($sql);
+        return pg_query($this->connection, $sql);
+    }
+
+    public function fetch_all($result)
+    {
+        return pg_fetch_all($result);
+    }
+
+    public function fetch_assoc($result)
+    {
+        return pg_fetch_assoc($result);
+    }
+
+    public function fetch_result($result)
+    {
+        return pg_fetch_result($result);
+    }
+}
